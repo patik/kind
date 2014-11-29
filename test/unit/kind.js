8dd9fecbe11f8e6496818cc6d804b64a08f73878
Craig Patik <craig@patik.com>
2014-11-29 12:59:31 -0500

    More tests

--- test/unit/kind.js
+++ test/unit/kind.js
@@ -1,8 +1,10 @@
-/*global $:true, kind:true, module:true, test:true, ok:true */
+/*global $:true, kind:true, module:true, test:true, ok:true, console:true */
 $(function() {
     'use strict';
 
     module('kind');
+
+    var testDiv = document.getElementById('comment-sibling');
 
     test('Standard types', function() {
         ok(kind('{}') === 'string', 'String');
@@ -30,36 +32,72 @@ $(function() {
         ok(kind('str') !== 'array', 'String with `length` not considered an array');
     });
 
+    test('Array-like objects', function() {
+        ok(kind(arguments) === 'arraylike', 'Arguments');
+        ok(kind([1,2,3]) !== 'arraylike', 'Standard array not considered arraylike');
+        ok(kind(document.getElementsByTagName('div')) !== 'arraylike', 'Nodelist not considered arraylike');
+    });
+
     test('Built-in special objects', function() {
         ok(kind(new Date()) === 'date', 'Date');
         ok(kind(/regular\sexpression/) === 'regexp', 'Regular expression (literal)');
         ok(kind(new RegExp('regular\\sexpression')) === 'regexp', 'Regular expression (constructor)');
         ok(kind(new Error('err')) === 'error', 'Error');
         ok(kind(Math) === 'math', 'Math');
+
+        if (navigator.userAgent.indexOf('PhantomJS') === -1) {
+            if (testDiv.addEventListener) {
+                testDiv.addEventListener('click', function(evt) {
+                    ok(kind(evt) === 'event', 'Event');
+                }, false);
+
+                testDiv.click();
+            }
+            else {
+                testDiv.attachEvent('onclick', function(evt) {
+                    ok(kind(evt) === 'event', 'Event');
+                });
+
+                testDiv.onclick();
+            }
+        }
     });
 
     test('Elements and nodes', function() {
-        var testDiv = document.getElementById('comment-sibling'),
-            elems = document.getElementsByTagName('*');
-
-        console.log(kind(elems) === 'nodelist');
+        var elems = document.getElementsByTagName('*');
 
         ok(kind(elems[0]) === 'element', 'Single item from nodelist detected as an element');
-        ok(kind(elems) === 'nodelist', 'getElementsByTagName\'s results are detected as a nodelist');
-        ok(kind(testDiv) === 'element', 'Div detected as an element');
-        ok(kind(testDiv.nextSibling) === 'node', 'Comment detected as a node');
+        ok(kind(testDiv) === 'element', 'getElementById result is an element');
+
+        // PhantomJS returns an incorrect value for this test, but it works correctly in browsers
+        if (navigator.userAgent.indexOf('PhantomJS') !== -1) {
+            // PhantomJS
+            ok(kind(elems) === 'function', 'Result of getElementsByTagName is a nodelist');
+        }
+        else {
+            // Browser
+            ok(kind(elems) === 'nodelist', 'Result of getElementsByTagName is a nodelist');
+        }
+
+        ok(kind(document.body) === 'element', 'document.body is an element');
+        ok(kind(document.documentElement) === 'element', 'document.documentElement is an element');
         ok(kind(document.getElementsByTagName('nonexistent')[0]) === 'undefined', 'Empty nodelist item detected as undefined');
     });
 
-    test('Plain object', function() {
-        var divs = document.getElementsByTagName('div');
+    test('Elements and nodes (deep)', function() {
+        ok(kind(testDiv.nextSibling, true) === 'comment', 'Comment is detected');
+        ok(kind(document, true) === 'document', 'document is a node');
+    });
 
-        ok(kind({}) === 'object', 'Standard empty object properly detected');
-        ok(kind({foo:'bar'}) === 'object', 'Standard object properly detected');
+    test('Plain object', function() {
+        var elems = document.getElementsByTagName('*');
+
+        ok(kind({foo:'bar'}) === 'object', 'Standard object declaration');
+        ok(kind({}) === 'object', 'Standard object declaration (empty)');
+        ok(kind(new Object()) === 'object', 'Object constructor');
         ok(kind([1,2]) !== 'object', 'Array not considered an object');
-        ok(kind(new Object()) === 'object', 'Object constructor detected');
-        ok(kind(divs) !== 'object', 'Nodelist not considered an object');
-        ok(kind(divs[0]) !== 'object', 'Element not considered an array');
-        ok(kind(document.createTextNode('')) !== 'object', 'Text node not considered an array');
+        ok(kind(elems) !== 'object', 'Nodelist not considered an object');
+        ok(kind(elems[0]) !== 'object', 'Element not considered an object');
+        ok(kind(document.createTextNode('')) !== 'object', 'Text node not considered an object');
     });
 });
