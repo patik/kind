35134b218216494fef7b925e5d38b9c1589b8e09
Craig Patik <craig@patik.com>
2014-11-29 11:25:10 -0500

    Added some unit tests

--- test/unit/kind.js
+++ test/unit/kind.js
@@ -2,96 +2,64 @@
 $(function() {
     'use strict';
 
-    module('kind plugin');
+    module('kind');
 
-    test('Arrays', function() {
-        var divs = document.getElementsByTagName('div');
-
-        ok(kind([1,2,3]) === 'array', 'Standard array properly detected');
-        ok(kind(divs) !== 'array', 'Nodelist not considered an array');
+    test('Standard types', function() {
+        ok(kind('{}') === 'string', 'String');
+        ok(kind('') === 'string', 'String (empty)');
+        ok(kind() === 'undefined', 'Undefined');
+        ok(kind(function(){}) === 'function', 'Anonymous function');
+        ok(kind(kind) === 'function', 'Defined function');
+        ok(kind(document.getElementById) === 'function', 'Method');
+        ok(kind(false) === 'boolean', 'Boolean');
+        ok(kind(!null) === 'boolean', 'Boolean (coerced)');
+        ok(kind(1) === 'number', 'Number (integer)');
+        ok(kind(1.21) === 'number', 'Number (float)');
     });
 
-    // module('affix', {
-    //     setup: function() {
-    //         // Run all tests in noConflict mode -- it's the only way to ensure that the plugin works in noConflict mode
-    //         $.fn.bootstrapAffix = $.fn.affix.noConflict()
-    //     },
-    //     teardown: function() {
-    //         $.fn.affix = $.fn.bootstrapAffix
-    //         delete $.fn.bootstrapAffix
-    //     }
-    // })
+    test('Null', function() {
+        ok(kind(null) === 'null', 'Literal null');
+        ok(kind(document.getElementById('nonexistent')) === 'null', 'No element found with given ID');
+    });
 
-    // test('should provide no conflict', function() {
-    //     strictEqual($.fn.affix, undefined, 'affix was set back to undefined (org value)')
-    // })
+    test('Array', function() {
+        ok(kind([1,2,3]) === 'array', 'Standard array properly detected');
+        ok(kind([]) === 'array', 'Empty array properly detected');
+        ok(kind(new Array(1)) === 'array', 'Array constructor detected');
+        ok(kind(document.getElementsByTagName('div')) !== 'array', 'Nodelist not considered an array');
+        ok(kind('str') !== 'array', 'String with `length` not considered an array');
+    });
 
-    // test('should return jquery collection containing the element', function() {
-    //     var $el = $('<div/>')
-    //     var $affix = $el.bootstrapAffix()
-    //     ok($affix instanceof $, 'returns jquery collection')
-    //     strictEqual($affix[0], $el[0], 'collection contains element')
-    // })
+    test('Built-in special objects', function() {
+        ok(kind(new Date()) === 'date', 'Date');
+        ok(kind(/regular\sexpression/) === 'regexp', 'Regular expression (literal)');
+        ok(kind(new RegExp('regular\\sexpression')) === 'regexp', 'Regular expression (constructor)');
+        ok(kind(new Error('err')) === 'error', 'Error');
+        ok(kind(Math) === 'math', 'Math');
+    });
 
-    // test('should exit early if element is not visible', function() {
-    //     var $affix = $('<div style="display: none"/>').bootstrapAffix()
-    //     $affix.data('bs.affix').checkPosition()
-    //     ok(!$affix.hasClass('affix'), 'affix class was not added')
-    // })
+    test('Elements and nodes', function() {
+        var testDiv = document.getElementById('comment-sibling'),
+            elems = document.getElementsByTagName('*');
 
-    // test('should trigger affixed event after affix', function() {
-    //     stop()
+        console.log(kind(elems) === 'nodelist');
 
-    //     var templateHTML = '<div id="affixTarget">' + '<ul>' + '<li>Please affix</li>' + '<li>And unaffix</li>' + '</ul>' + '</div>' + '<div id="affixAfter" style="height: 20000px; display: block;"/>'
-    //     $(templateHTML).appendTo(document.body)
+        ok(kind(elems[0]) === 'element', 'Single item from nodelist detected as an element');
+        ok(kind(elems) === 'nodelist', 'getElementsByTagName\'s results are detected as a nodelist');
+        ok(kind(testDiv) === 'element', 'Div detected as an element');
+        ok(kind(testDiv.nextSibling) === 'node', 'Comment detected as a node');
+        ok(kind(document.getElementsByTagName('nonexistent')[0]) === 'undefined', 'Empty nodelist item detected as undefined');
+    });
 
-    //     $('#affixTarget').bootstrapAffix({
-    //         offset: $('#affixTarget ul').position()
-    //     })
+    test('Plain object', function() {
+        var divs = document.getElementsByTagName('div');
 
-    //     $('#affixTarget')
-    //         .on('affix.bs.affix', function() {
-    //             ok(true, 'affix event fired')
-    //         }).on('affixed.bs.affix', function() {
-    //             ok(true, 'affixed event fired')
-    //             $('#affixTarget, #affixAfter').remove()
-    //             start()
-    //         })
-
-    //     setTimeout(function() {
-    //         window.scrollTo(0, document.body.scrollHeight)
-
-    //         setTimeout(function() {
-    //                 window.scroll(0, 0)
-    //             }, 16) // for testing in a browser
-    //     }, 0)
-    // })
-
-    // test('should affix-top when scrolling up to offset when parent has padding', function() {
-    //     stop()
-
-    //     var templateHTML = '<div id="padding-offset" style="padding-top: 20px;">' + '<div id="affixTopTarget">' + '<p>Testing affix-top class is added</p>' + '</div>' + '<div style="height: 1000px; display: block;"/>' + '</div>'
-    //     $(templateHTML).appendTo(document.body)
-
-    //     $('#affixTopTarget')
-    //         .bootstrapAffix({
-    //             offset: {
-    //                 top: 120,
-    //                 bottom: 0
-    //             }
-    //         })
-    //         .on('affixed-top.bs.affix', function() {
-    //             ok($('#affixTopTarget').hasClass('affix-top'), 'affix-top class applied')
-    //             $('#padding-offset').remove()
-    //             start()
-    //         })
-
-    //     setTimeout(function() {
-    //         window.scrollTo(0, document.body.scrollHeight)
-
-    //         setTimeout(function() {
-    //             window.scroll(0, 119)
-    //         }, 250)
-    //     }, 250)
-    // })
+        ok(kind({}) === 'object', 'Standard empty object properly detected');
+        ok(kind({foo:'bar'}) === 'object', 'Standard object properly detected');
+        ok(kind([1,2]) !== 'object', 'Array not considered an object');
+        ok(kind(new Object()) === 'object', 'Object constructor detected');
+        ok(kind(divs) !== 'object', 'Nodelist not considered an object');
+        ok(kind(divs[0]) !== 'object', 'Element not considered an array');
+        ok(kind(document.createTextNode('')) !== 'object', 'Text node not considered an array');
+    });
 });
