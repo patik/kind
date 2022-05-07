efc31eac68ff0eaca1ca08f375521b123ace7572
Craig Patik <craig@patik.com>
2022-05-07 17:50:30 +0200

    trying to fix CI

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
+    testEnvironment: 'jest-environment-jsdom',
     testMatch: ['**/test/**/*.spec.ts'],
     collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/types/**/*.ts'],
     globals: {
