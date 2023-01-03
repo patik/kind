3fa5e1bf77c6ffa67163926861fbfe95a5a8c9ac
Craig Patik <craig@patik.com>
2023-01-03 09:38:58 +0100

    try to fix CI linting

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,8 @@ module.exports = {
     testMatch: ['**/test/**/*.spec.ts'],
     collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/types/**/*.ts'],
     globals: {
-        'ts-jest': {
-            diagnostics: false,
-            isolatedModules: true,
+        transform: {
+            '^.+\\.tsx?$': ['ts-jest', { diagnostics: false, isolatedModules: true }],
         },
     },
 }
